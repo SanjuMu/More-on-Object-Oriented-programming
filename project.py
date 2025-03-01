799f797b373594ad8e33d4b95d08e156d2be1a3d
SanjuMu <sanjusri.munigala@gmail.com>
2025-03-01 12:15:44 -0500

    project

--- project.py
+++ project.py
@@ -1,17 +1,17 @@
+import math
+
 class Circle:
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        import math
         return math.pi * (self.radius ** 2)
 
     def perimeter(self):
-        import math
         return 2 * math.pi * self.radius
+    
+radius_value = float(input("Enter the radius of the circle: "))
 
-
-circle = Circle(5)
-print("Area:", circle.area())
-print("Perimeter:", circle.perimeter())
-
+circle = Circle(radius_value)
+print("Area of the circle:", circle.area())
+print("Perimeter of the circle:", circle.perimeter())
